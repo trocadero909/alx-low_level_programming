4df199697a46981e6577b0ba1d7f4f0ab3fb0cf2
trocadero909 <trocadero909@gmail.com>
2023-09-13 17:34:19 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#inlude "main.h"
+
+/**
+ * print_alphabet_10 - function that will print  the alphabet 10 times 
+ *
+ * */
+ void print_alphabet_x10(void);
+{
+        int line. ch;
+        for (line = 0; line <= 9; line++)
+	{
+		for (ch = 'a'; ch <= 'Z'; ch++)
+                _putchar[ch]);
+        _putchar('\n');
+       }}
+
