7de4e94a59388030eb03d398a3977466c72f3e76
trocadero909 <trocadero909@gmail.com>
2023-09-13 17:21:18 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#inlude "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+ * */
+ void print_alphabet(void);
+{
+        int ch;
+
+        for (ch = 'a'; ch < 'z'; ch++)
+                _putchar(str[ch]);
+        _putchar('\n');
+        return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,9 @@
-#include <unistd.h>
-#inlude "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _putchar - writes the charcter c to stdout
- * @c: The character to print 
- *
- * Return: On success 1.
- *         On error. -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c) {
-    return write(1, &c, 1);
+#inlude <stdio.h>
+
+int _putchar(char c);
+void print_alphabet(void);
+
+#endif 
